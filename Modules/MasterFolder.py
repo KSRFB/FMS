c858102b9573db48cd89e07f7c787541648cb0a6
KSRFB <fabien.viallevieille@gmail.com>
2015-01-24 15:46:59 +0000

    Master Folder Class Methods
    Created list of methods to create

--- Modules/MasterFolder.py
+++ Modules/MasterFolder.py
@@ -1,1 +1,12 @@
 #! Master Folder
+Class MasterFolder(name,path):
+    MasterFolder.name=name
+    MasterFolder.path=path
+
+
+    #def checkFolder
+    #update MasterFolderList
+    #def createFolder
+    #def createDB
+    #def getinfo
+    
