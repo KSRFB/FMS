7464a82e811b7a591b9009235dafb80cf102f94c
KSRFB <fabien.viallevieille@gmail.com>
2015-01-24 20:19:51 +0000

    MasterFolder creation code
    This is the code to create the Master Folders, save MasterFolder path in
    database and save Folders details

--- Modules/OtherFunc.py
+++ Modules/OtherFunc.py
@@ -12,11 +12,66 @@ def TimeDisplay(datetime):
     s='0' + str(datetime.second)
     s=s[-2:]
     dstring = '%s/%s/%s - %s:%s:%s' %(d,m,y,h,mi,s)
-
     return dstring
 
 
+def createMF(name,path):
+    name=name.strip()
+    dir=os.path.join(path,name)
+    db = shelve.open('MasterFolderdb')
+    try:
+        db[dir]
+    except:
+        db[dir]=dir
+    db.close()
+
+    try:
+        os.stat(dir)
+    except:
+        os.mkdir(dir)
+    dir2=os.path.join(dir,name + 'db')
+    db2 = shelve.open(dir2)
+    try:
+        db2['title']
+    except:
+        title = name.strip()
+        db2['title']=title
+
+    try:
+        db2['description']
+    except:
+        desc = input('Description?')
+        db2['description']=desc
+
+    try:
+        db2['LET']
+    except:
+        LET=input('Enter the starting characters for your folder (e.g. ENG)').strip().upper()
+        a=True
+        while a:
+            try:
+                folder=os.path.join(dir,LET+'0000000000000')
+                os.mkdir(folder)
+                os.rmdir(folder)
+                a = False
+            except:
+                LET=input(folder + 'A folder cannot be created using these character. Enter other starting characters for your folder (e.g. ENG)').strip().upper()
+        db2['LET']= LET
+        db2.close()
+        
+            
+    
+
+    
+    
+
 if __name__ == '__main__':
-    import datetime
-    d=datetime.datetime.now()
-    date = timedisplay(d)
+    import os
+    import shelve
+    from datetime import *
+    name='Human Resource'
+    path=r'C:\Users\fviallevieille\PythonTest'
+    d=datetime.now()
+    date = TimeDisplay(d)
+    createMF(name,path)
+    
