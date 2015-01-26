cfbf1ecedee6e2bce478305ed36c36fd64cb98c1
KSRFB <fabien.viallevieille@gmail.com>
2015-01-26 23:23:41 +0000

    update
    update

--- Main.py
+++ Main.py
@@ -1,33 +1,53 @@
 from tkinter import *
 from tkinter import ttk
 import shelve
+
+
+#Create and Edit Window
 root=Tk()
-#root.title="Folder Management System"
-#root.update
-
 root.title('Folder Management System')
-Content=ttk.Frame(root)
-Folderlbl=ttk.Label(Content, text="Folder: ")
 
+
+#Create first Frame 'Content1'
+Content1=ttk.Frame(root)
+
+#Edit 'Content1' Label
+Folderlbl=ttk.Label(Content1, text="Folder: ")
+
+
+
+#Fetch 'Content 1' Combobox Content
 db=shelve.open('FolderDB')
 keys=[]
 Folder=[]
 FolderCombo=[]
+ComboList=StringVar()
 for item in db:
     keys.append(item)
     Folder.append(db[item])
     FolderCombo.append(db[item][0].upper() + ' in ' + db[item][1])
 
+ComboList.set(FolderCombo)
 
-
-Folder = ttk.Combobox(Content)
-Folder['values']=FolderCombo
+#Edit 'Content1' Combobox
+Folder = ttk.Combobox(Content1)
 Folder['width']=50
+Folder['values']= FolderCombo
 
-NewButton=ttk.Button(Content,text='New')
-ProjectFrame=ttk.Frame(Content)
+
+
+#Create button to create new Folder
+NewButton=ttk.Button(Content1,text='New')
+
+#Create Description Label
+
+#Create second frame 'Content2'
+
+Content2=ttk.Frame(root)
+
+#create Projects Variable 
 Projects=StringVar()
-ProjectLabel=ttk.Label(ProjectFrame,textvariable=Projects)
+ProjectLabel=ttk.Label(Content2,textvariable=Projects)
 Projects.set('Selecte a Folder')
 
 
@@ -36,11 +56,11 @@ Projects.set('Selecte a Folder')
 
 
 
-Content.grid(column=0,row=0)
+Content1.grid(column=0,row=0)
 Folderlbl.grid(column=0,row=0, sticky=(N,E),padx=5,pady=5)
 Folder.grid(column=1,row=0,rowspan=2,sticky=(N,W),padx=5,pady=5)
 NewButton.grid(column=3,row=0,sticky=(N,W),padx=5,pady=3)
-ProjectFrame.grid(column=0,row=1,columnspan=4,sticky=N)
+Content2.grid(column=0,row=1,columnspan=4,sticky=N)
 ProjectLabel.grid(column=0,row=0,columnspan=4,sticky=N)
 Folder.bind('<<ComboboxSelected>>',lambda e: Projects.set(Folder.get()))
 
